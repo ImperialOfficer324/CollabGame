07b9063d5fc373b5c143e52e6135243fbf69246a
Keaton Auld <keaton.auld@gmail.com>
2021-11-03 17:08:35 -0400

    removed debug messages

--- source/client.py
+++ source/client.py
@@ -2,9 +2,11 @@ import socket
 
 server_address=("localhost", 6789)
 max_size=1000
-print("starting the client")
 client=socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(server_address)
-client.sendall(b"Hello World")
 data=client.recv(max_size)
+
+
+
+
 client.close()
--- source/server.py
+++ source/server.py
@@ -3,8 +3,6 @@ import socket
 address = ('localhost',6789)
 max_size = 1000
 
-print("starting the server")
-
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind(address)
 
@@ -13,8 +11,9 @@ server.listen(5)
 client,addr = server.accept()
 data = client.recv(max_size)
 
-print(data)
 
-client.sendall(b'Hullo')
+
+
+
 client.close()
 server.close()
