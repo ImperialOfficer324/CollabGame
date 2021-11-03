f70c08f86189ad06e3c6f05d64875e3cd6e14099
Keaton Auld <keaton.auld@gmail.com>
2021-11-03 17:01:43 -0400

    fixed server

--- source/server.py
+++ source/server.py
@@ -1,16 +1,20 @@
 import socket
 
-server_address = ("localhost",6789)
+address = ('localhost',6789)
 max_size = 1000
 
-print("Starting the Server")
-server = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-server.bind(server_address)
+print("starting the server")
+
+server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server.bind(address)
+
+server.listen(5)
 
 client,addr = server.accept()
 data = client.recv(max_size)
 
-print(f"{client} says {data}")
-client.sendall(b"hello")
+print(data)
+
+client.sendall(b'Hullo')
 client.close()
 server.close()
